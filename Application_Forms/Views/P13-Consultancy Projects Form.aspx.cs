468894dc4dd427671f1820e1f9619c787545c0b6
Saoni Deb <saonideb@gmail.com>
2021-10-02 11:28:32 +0530

    added Form 13

---
+++ Application_Forms/Views/P13-Consultancy Projects Form.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Individual_Users_P13_Consultancy_Projects_Form : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lbl_current_date.Text = Convert.ToDateTime(System.DateTime.Now.ToShortDateString()).ToString("dd/MM/yyyy");
+    }
+}
